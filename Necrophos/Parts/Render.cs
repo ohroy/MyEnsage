232353a16ab6577c22dfb02b913cfeed30771859
Rozbo <75200306@qq.com>
2019-01-23 01:10:55 +0800

    rework the hpbar damage calculation

--- Necrophos/Parts/Render.cs
+++ Necrophos/Parts/Render.cs
@@ -6,8 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Ensage;
 using Ensage.Common;
+using Ensage.SDK.Renderer;
 using Ensage.SDK.Service;
 using SharpDX;
+using Color = System.Drawing.Color;
+
 
 namespace wtf.Parts
 {
@@ -19,25 +22,30 @@ namespace wtf.Parts
         private Satellite _satellite;
         [Import("menu")]
         private Menu _menu;
+
+        private readonly IRendererManager renderer;
+
+
         [ImportingConstructor]
         public Render([Import] IServiceContext context)
         {
             //Owner = context.Owner as Hero; ;
+            renderer = context.Renderer;
         }
 
 
         public void Install()
         {
-            Drawing.OnDraw += OnDraw;
+            renderer.Draw += OnDraw;
         }
 
         public void Uninstall()
         {
-            Drawing.OnDraw -= OnDraw;
+            renderer.Draw -= OnDraw;
         }
-        private void Text(string text, Vector2 pos, Color color)
+        private void Text(string text, Vector2 pos, System.Drawing.Color color)
         {
-            Drawing.DrawText(text, "Arial", pos, new Vector2(21), color, FontFlags.None);
+            renderer.DrawText(pos, text, color,13f, "Arial");
         }
 
         private void showHpBar()
@@ -51,20 +59,25 @@ namespace wtf.Parts
                 {
                     var hpBarSizeX = HUDInfo.GetHPBarSizeX(target);
                     var hpBarSizeY = HUDInfo.GetHpBarSizeY(target) / 1.7f;
-                    var hpBarPos = hpBarPosition + new Vector2(0, hpBarSizeY * (84 / 70f));
+                    var hpBarPos = hpBarPosition + new Vector2(1, hpBarSizeY * (84 / 70f));
 
                     var health = data.GetHealth;
                     var readyDamage = data.GetReadyDamage;
                     var readyDamageBar = Math.Max(readyDamage, 0) / target.MaximumHealth;
+
                     if (readyDamageBar > 0)
                     {
+                        //剩余和最大生命值的比例
                         var readyDamagePos = Math.Max(health - readyDamage, 0) / target.MaximumHealth;
-                        var readyDamagePosition = new Vector2(hpBarPos.X + ((hpBarSizeX + readyDamageBar) * readyDamagePos), hpBarPos.Y);
+                        //计算剩余血量终点的位置，刚好是预计伤害的起点
+                        var readyDamagePosition = new Vector2(hpBarPos.X + hpBarSizeX * readyDamagePos, hpBarPos.Y);
+                        //计算预计伤害的长度hpBarSizeX*readyDamageBar，防止溢出,要减去溢出的伤害血量
+                        //这是一种取巧的写法，普通应该是判断溢出直接等同于当前血量的终点
                         var readyDamageSize = new Vector2(hpBarSizeX * (readyDamageBar + Math.Min(health - readyDamage, 0) / target.MaximumHealth), hpBarSizeY);
-                        var readyDamageColor = ((float)health / target.MaximumHealth) - readyDamageBar > 0 ? new Color(100, 0, 0, 200) : new Color(191, 255, 0, 200);
-
-                        Drawing.DrawRect(readyDamagePosition, readyDamageSize, readyDamageColor);
-                        Drawing.DrawRect(readyDamagePosition, readyDamageSize, Color.Black, true);
+                        var readyDamageColor = (readyDamage-health) < 0 ? Color.FromArgb(200, 100, 0,0 ) : Color.FromArgb(200,191, 255, 0);
+                        var rect = new RectangleF(readyDamagePosition.X, readyDamagePosition.Y, readyDamageSize.X,
+                            readyDamageSize.Y);
+                        renderer.DrawFilledRectangle(rect, Color.Black,readyDamageColor,1f);
                     }
 
                     var damage = data.GetDamage;
@@ -72,18 +85,18 @@ namespace wtf.Parts
                     if (damageBar > 0)
                     {
                         var damagePos = Math.Max(health - damage, 0) / target.MaximumHealth;
-                        var damagePosition = new Vector2(hpBarPos.X + ((hpBarSizeX + damageBar) * damagePos), hpBarPos.Y);
+                        var damagePosition = new Vector2(hpBarPos.X + (hpBarSizeX * damagePos), hpBarPos.Y);
                         var damageSize = new Vector2(hpBarSizeX * (damageBar + Math.Min(health - damage, 0) / target.MaximumHealth), hpBarSizeY);
-                        var damageColor = ((float)health / target.MaximumHealth) - damageBar > 0 ? new Color(0, 255, 0) : Color.Aqua;
-
-                        Drawing.DrawRect(damagePosition, damageSize, damageColor);
-                        Drawing.DrawRect(damagePosition, damageSize, Color.Black, true);
+                        var damageColor = (damage - health) <0? Color.FromArgb(255,0, 255, 0) : Color.Aqua;
+                        var rect = new RectangleF(damagePosition.X, damagePosition.Y, damageSize.X,
+                            damageSize.Y);
+                        renderer.DrawFilledRectangle(rect,Color.Black, damageColor,1f);
                     }
                 }
             }
         }
 
-        private void OnDraw(EventArgs args)
+        private void OnDraw(object sender, EventArgs eventArgs)
         {
             if (_menu.IsShowHpBarEnabled)
             {
