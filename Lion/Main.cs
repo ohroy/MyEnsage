8a51071f74a229d8e501ed74ad64bb41ceef5a23
Rozbo <75200306@qq.com>
2019-01-27 00:25:27 +0800

    fix the sdk bug that ult does not check AghanimsScepter

--- Lion/Main.cs
+++ Lion/Main.cs
@@ -8,7 +8,7 @@ using ParticleManager = wtf.lion.Parts.ParticleManager;
 
 namespace wtf.lion
 {
-    [ExportPlugin(name: "wtf.lion", author: "rozbo", version: "1.4.0.0", units: HeroId.npc_dota_hero_lion)]
+    [ExportPlugin(name: "wtf.lion", author: "rozbo", version: "1.4.1.0", units: HeroId.npc_dota_hero_lion)]
     internal class Lion : Plugin
     {
 
